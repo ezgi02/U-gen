631d6cc6b961c2cfdf1e803fe6b58de7edc2fe83
Ezgi Adıgüzel <ezgiadiguzel01@gmail.com>
2023-10-31 17:33:37 +0300

    Add project files.

---
+++ Uçgen/Program.cs
@@ -0,0 +1,30 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+
+public class TriangleDrawer
+{
+    public void DrawTriangle(int size)
+    {
+        for (int i = 1; i <= size; i++)
+        {
+            for (int j = 1; j <= i; j++)
+            {
+                Console.Write("* ");
+            }
+            Console.WriteLine();
+        }
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.Write("Üçgenin boyutunu girin: ");
+        int size = int.Parse(Console.ReadLine());
+
+        TriangleDrawer triangleDrawer = new TriangleDrawer();
+        triangleDrawer.DrawTriangle(size);
+    }
+}
+
